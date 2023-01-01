965073bb65c7b0882aeb5495c026fe1ea85c1e7d
FaizalKhan-AT <faizalkhanfk148@gmail.com>
2023-01-01 19:47:06 +0530

    react-snap 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthContext from "./contexts/AuthContext";
@@ -9,36 +9,23 @@ import SearchContext from "./contexts/SearchContext";
 import { hydrate, render } from "react-dom";
 import "./index.css";
 const root = document.getElementById("root") as HTMLElement;
+const APP = (
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContext>
+        <PostContext>
+          <SearchContext>
+            <App />
+          </SearchContext>
+        </PostContext>
+      </AuthContext>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 if (root.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <BrowserRouter>
-        <AuthContext>
-          <PostContext>
-            <SearchContext>
-              <App />
-            </SearchContext>
-          </PostContext>
-        </AuthContext>
-      </BrowserRouter>
-    </React.StrictMode>,
-    root
-  );
+  hydrate(APP, root);
 } else {
-  render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <AuthContext>
-          <PostContext>
-            <SearchContext>
-              <App />
-            </SearchContext>
-          </PostContext>
-        </AuthContext>
-      </BrowserRouter>
-    </React.StrictMode>,
-    root
-  );
+  render(APP, root);
 }
 // ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 //   <React.StrictMode>
